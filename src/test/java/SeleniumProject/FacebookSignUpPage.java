4513d3399ffae27b974aa74b8a9ee6401cad3a7d
shtefanuz <s-nuzhdenko@mail.ru>
2023-02-11 09:33:16 -0600

    Pom was added

--- src/test/java/SeleniumProject/FacebookSignUpPage.java
+++ src/test/java/SeleniumProject/FacebookSignUpPage.java
@@ -8,15 +8,23 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
 public class FacebookSignUpPage {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         WebDriverManager.chromedriver().setup();
 
         WebDriver driver = new ChromeDriver();
 
         driver.manage().window().maximize();
-        // TC1
+        // TC1: 1) Go to Facebook Page, 2) Click to create new account
         driver.get("https://www.facebook.com/");
+
+        //<a role="button" class="_42ft _4jy0 _6lti _4jy6 _4jy2 selected _51sy" href="#"
+        // ajaxify="/reg/spotlight/" id="u_0_0_vI" data-testid="open-registration-form-button"
+        // rel="async">Create new account</a>
+        Thread.sleep(3000);
+        WebElement createNewAccount = driver.findElement(By.className("_42ft _4jy0 _6lti _4jy6 _4jy2 selected _51sy"));
+        createNewAccount.click();
+
 
     }
 }
